f3733b056d5263b30284f26474a4e66e6e36116d
Mayaputri04 <mayaputri.nurfajri@gmail.com>
2025-05-19 11:15:14 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import { addNoteHandler } from './handler';
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+];
+export default routes;
---
+++ src/server.js
@@ -0,0 +1,12 @@
+import { server as _server } from '@hapi/hapi';
+import routes from './routes';
+const init = async () => {
+  const server = _server({
+    port: 5000,
+    host: 'localhost',
+  });
+  server.route(routes);
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+init();
