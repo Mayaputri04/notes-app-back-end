80c1b7ddc6109bbf1d477740ef46392a5c5afd9a
Mayaputri04 <mayaputri.nurfajri@gmail.com>
2025-05-19 12:11:38 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 import { server as _server } from '@hapi/hapi';
 import routes from './routes';
 const init = async () => {
-  const server = _server({
+  const server = Hapi.server({    
     port: 5000,
-    host: 'localhost',
-  });
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+        cors: {
+            origin: ['*'],
+        },
+    },
+});;
   server.route(routes);
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
